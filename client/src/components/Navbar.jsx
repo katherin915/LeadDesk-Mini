5f266701237e64cf25768dae3717017cebaaf5d8
Katherin Pandey <katherinpandey@gmail.com>
2026-07-25 01:05:16 +0530

    Add navigation to login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar">
       <h2 className="logo">
         LeadDesk <span>Mini</span>
       </h2>
 
-      <button className="login-btn">
+      <button
+        className="login-btn"
+        onClick={() => navigate("/login")}
+      >
         Dashboard Login
       </button>
     </nav>
